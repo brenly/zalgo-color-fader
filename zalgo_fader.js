ab8aa43877f12f0e991bb51defa4c85e55dcc274
brenly <brenlyd@gmail.com>
2018-11-16 15:58:55 -0600

    the code compiles

--- zalgo_fader.js
+++ zalgo_fader.js
@@ -1,4 +1,4 @@
-﻿import Math;
+//import Math;
 
 //zalgo html color fader by Brenly github.com/brenly
 //code partials borrowed from:
@@ -12,8 +12,8 @@
 //https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_charat
 //https://mothereff.in/js-escapes#1%E6%BC%A2%E5%AD%97
 
-final char[] zalgo_up =
-    { '\u030d', /*     �?     */'\u030e', /*     ̎     */'\u0304', /*     ̄     */'\u0305', /*     ̅     */
+var zalgo_up =
+    [ '\u030d', /*     �?     */'\u030e', /*     ̎     */'\u0304', /*     ̄     */'\u0305', /*     ̅     */
         '\u033f', /*     ̿     */'\u0311', /*     ̑     */'\u0306', /*     ̆     */'\u0310', /*     �?     */
         '\u0352', /*     ͒     */'\u0357', /*     ͗     */'\u0351', /*     ͑     */'\u0307', /*     ̇     */
         '\u0308', /*     ̈     */'\u030a', /*     ̊     */'\u0342', /*     ͂     */'\u0343', /*     ̓     */
@@ -26,9 +26,9 @@ final char[] zalgo_up =
         '\u036a', /*     ͪ     */'\u036b', /*     ͫ     */'\u036c', /*     ͬ     */'\u036d', /*     ͭ     */
         '\u036e', /*     ͮ     */'\u036f', /*     ͯ     */'\u033e', /*     ̾     */'\u035b', /*     ͛     */
         '\u0346', /*     ͆     */'\u031a' /*     ̚     */
-        } ;
-final char[] zalgo_down =
-    { '\u0316', /*     ̖     */'\u0317', /*     ̗     */'\u0318', /*     ̘     */'\u0319', /*     ̙     */
+     ] ;
+var zalgo_down =
+    [ '\u0316', /*     ̖     */'\u0317', /*     ̗     */'\u0318', /*     ̘     */'\u0319', /*     ̙     */
         '\u031c', /*     ̜     */'\u031d', /*     �?     */'\u031e', /*     ̞     */'\u031f', /*     ̟     */
         '\u0331', /*     ̱     */'\u0332', /*     ̲     */'\u0333', /*     ̳     */'\u0339', /*     ̹     */
         '\u0320', /*     �      */'\u0324', /*     ̤     */'\u0325', /*     ̥     */'\u0326', /*     ̦     */
@@ -38,34 +38,35 @@ final char[] zalgo_down =
         '\u0347', /*     ͇     */'\u0348', /*     ͈     */'\u0349', /*     ͉     */'\u034d', /*     �?     */
         '\u034e', /*     ͎     */'\u0353', /*     ͓     */'\u0354', /*     ͔     */'\u0355', /*     ͕     */
         '\u0356', /*     ͖     */'\u0359', /*     ͙     */'\u035a', /*     ͚     */'\u0323' /*     ̣     */
-        } ;
+     ] ;
 
     //those always stay in the middle
-final char[] zalgo_mid =
-    { '\u0315', /*     ̕     */'\u031b', /*     ̛     */'\u0340', /*     ̀     */'\u0341', /*     �?     */
+var zalgo_mid =
+    [ '\u0315', /*     ̕     */'\u031b', /*     ̛     */'\u0340', /*     ̀     */'\u0341', /*     �?     */
         '\u0358', /*     ͘     */'\u0321', /*     ̡     */'\u0322', /*     ̢     */'\u0327', /*     ̧     */
         '\u0328', /*     ̨     */'\u0334', /*     ̴     */'\u0335', /*     ̵     */'\u0336', /*     ̶     */
         '\u034f', /*     �?     */'\u035c', /*     ͜     */'\u035d', /*     �?     */'\u035e', /*     ͞     */
         '\u035f', /*     ͟     */'\u0360', /*     �      */'\u0362', /*     ͢     */'\u0338', /*     ̸     */
         '\u0337', /*     ̷     */'\u0361', /*     ͡     */'\u0489' /*     ҉_     */
-        } ;
+     ] ;
 
 
-int rand(int max) {
-            return (int)Math.floor(Math.random() * max);
+function rand() {
+            return Math.floor(Math.random() * max);
         }
 
         //gets a random char from a zalgo char table
-
-char rand_zalgo(char[] array) {
+        //this function is getting deprecated in my rewrite
+/*
+function rand_zalgo() {
             int ind = (int)Math.floor(Math.random() * array.length);
             return array[ind];
         }
-
+*/
         //hide show element
         //lookup char to know if its a zalgo char or not
-
-boolean is_zalgo_char(char c) {
+/*
+function is_zalgo_char(char c) {
             for (int i = 0; i < zalgo_up.length; i++)
                 if (c == zalgo_up[i])
                     return true;
@@ -77,8 +78,8 @@ boolean is_zalgo_char(char c) {
                     return true;
             return false;
         }
-
-void color_calc () {
+*/
+function color_calc () {
   function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
   function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
   function hexToG(h) {return parsoutput_zalgoeInt((cutHex(h)).substring(2,4),16)}
@@ -92,24 +93,39 @@ void color_calc () {
       + "0123456789ABCDEF".charAt(n%16);
   }
 }
+/* OLD FUNCTION HEADER FOR SAFEKEEPING
+function he_comes(iText, zalgo_opt_mini, zalgo_opt_normal, zalgo_up, zalgo_down, zalgo_mid, color1, color2, colormid) {*/
 
-void he_comes(iText, boolean zalgo_opt_mini, boolean zalgo_opt_normal, boolean zalgo_up,
-                                     boolean zalgo_down, boolean zalgo_mid, color1, color2, colormid) {
+function he_comes() {
 
+            //block to get all the form data!
+            var iText = document.getElementById("iText").innerHTML;
+            //var iText = "342245254254253452"
+            //needs boolean retrieval
+            //var zalgo_opt_mini = document.getElementById("").innerHTML;
+            //var zalgo_opt_normal = document.getElementById("").innerHTML;
+            //var zalgo_opt_max = document.getElementById("").innerHTML;
+            //var zalgo_up = document.getElementById("").innerHTML;
+            //var zalgo_down = document.getElementById("").innerHTML;
+            //var zalgo_mid = document.getElementById("").innerHTML;
+            var color1 = document.getElementById("color1").innerHTML;
+            var color2 = document.getElementById("color2").innerHTML;
+            var colormid = document.getElementById("colormid").innerHTML;
+
+            //i forgot what this code does :(
             var output_colorized = document.getElementById('output_colorized');
             var output_html = document.getElementById('output_html');
-            int num_up;
-            int num_mid;
-            int num_down;
+            var num_up;
+            var num_mid;
+            var num_down;
 
             //this is no longer needed. there is no string to pass back. only a strict print to html div
             //String zalgoTxt = "";
 
-            for (int i = 0; i < iText.length(); i++) {
+            for (i = 0; i < iText.length(); i++) {
                 if (is_zalgo_char(iText.charAt(i)))
                     continue;
 
-Random rand = new Random();
 
                 //add the normal character
                 zalgoTxt += iText.charAt(i);
@@ -135,7 +151,7 @@ Random rand = new Random();
             //output_colorized.insertAdjacentHTML('beforeend', htmlcolorfunctin(insert));
             //output_html.insertAdjacentText('beforeend', htmlcolorfunctin(insert));
                 if (up)
-                    for (int j = 0; j < num_up; j++)
+                    for (j = 0; j < num_up; j++)
                       //IF VERTICAL == TRUE
                       // vertical color open
                         output_colorized.insertAdjacentText('beforeend', rand_zalgo(zalgo_up));
@@ -144,7 +160,7 @@ Random rand = new Random();
                         //output_colorized.insertAdjacentHTML('beforeend', "</FONT>");
                         //output_html.insertAdjacentText('beforeend', "</FONT>");
                 if (mid)
-                    for (int j = 0; j < num_mid; j++)
+                    for (j = 0; j < num_mid; j++)
                     //IF MIDSPECIAL == TRUE
                     //mid special color open
                     //IF VERTICAL == TRUE
@@ -158,7 +174,7 @@ Random rand = new Random();
                     //output_colorized.insertAdjacentHTML('beforeend', "</FONT>");
                     //output_html.insertAdjacentText('beforeend', "</FONT>");
                 if (down)
-                    for (int j = 0; j < num_down; j++)
+                    for (j = 0; j < num_down; j++)
                     //vertical color open
                         output_colorized.insertAdjacentText('beforeend', rand_zalgo(zalgo_down));
                         output_html.insertAdjacentText('beforeend', rand_zalgo(zalgo_down));
@@ -180,16 +196,12 @@ Random rand = new Random();
                     "Invoking the feeling of chaos.\n" +
                     "With out order.\n" +
                     "The Nezperdian hive-mind of chaos. Zalgo.    \n" +
-          has merge conflicts.has merge conflicts.
-
-To see the beginning of the merge conflict in your file, search the file for the conflict marker <<<<<<<. When you open the file in your text editor, you'll see the changes from the HEAD or base branch after the line <<<<<<< HEAD. Next, you'll see =======, which divides your changes from the changes in the other branch, followed by >>>>>>> BRANCH-NAME. In this example, one person wrote "open an issue" in the base or HEAD branch and another person wrote "ask your question in IRC" in the compare branch or branch-a.
-
-To see the beginning of the merge conflict in your file, search the file for the conflict marker <<<<<<<. When you open the file in your text editor, you'll see the changes from the HEAD or base branch after the line <<<<<<< HEAD. Next, you'll see =======, which divides your changes from the changes in the other branch, followed by >>>>>>> BRANCH-NAME. In this example, one person wrote "open an issue" in the base or HEAD branch and another person wrote "ask your question in IRC" in the compare branch or branch-a.He          "He who Waits Behind The Wall.\n" +
+                    "He who Waits Behind The Wall.\n" +
                     "ZALGO!", true, false, true, true, true);
                   }
-      */
-}
 
+//}
+*/
 
 /*
 //calls different color schemes horizontal / vertical
@@ -217,11 +229,4 @@ function toHex(n) {
     + "0123456789ABCDEF".charAt(n%16);
 }
 //END BORROWED BLOCK OF CODE
-*/
-
-var input_effect="";
-var input_color1="";
-var input_color2="";
-var input_color3="";
-
 */
